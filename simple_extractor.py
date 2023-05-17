b2f64fbfce2b19a78fef66fa2c5ab7d492f50b72
Claude Miao <yunliangmiao@gmail.com>
2023-05-17 17:54:04 +0800

    feat: 抽离方法

--- simple_extractor.py
+++ simple_extractor.py
@@ -25,6 +25,8 @@ import networks
 from utils.transforms import transform_logits
 from datasets.simple_extractor_dataset import SimpleFolderDataset
 
+from datasets.simple_image_extractor import SimpleImageDataset
+
 dataset_settings = {
     'lip': {
         'input_size': [473, 473],
@@ -58,11 +60,13 @@ def get_arguments():
     parser = argparse.ArgumentParser(description="Self Correction for Human Parsing")
 
     parser.add_argument("--dataset", type=str, default='lip', choices=['lip', 'atr', 'pascal'])
-    parser.add_argument("--model-restore", type=str, default='', help="restore pretrained model parameters.")
+    parser.add_argument("--model-restore", type=str, default='checkpoints/final.pth', help="restore pretrained model parameters.")
     parser.add_argument("--gpu", type=str, default='0', help="choose gpu device.")
-    parser.add_argument("--input-dir", type=str, default='', help="path of input image folder.")
-    parser.add_argument("--output-dir", type=str, default='', help="path of output image folder.")
+    parser.add_argument("--input-dir", type=str, default='inputs', help="path of input image folder.")
+    parser.add_argument("--output-dir", type=str, default='outputs', help="path of output image folder.")
     parser.add_argument("--logits", action='store_true', default=False, help="whether to save the logits.")
+    parser.add_argument("--image-name", type=str, default='123.jpg', help="whether to save the logits.")
+
 
     return parser.parse_args()
 
@@ -102,6 +106,14 @@ def get_palette(num_cls, indexs):
 
 
 def main():
+    # test
+    img = Image.open('inputs/123.jpg').convert('RGBA')
+
+    result = parse_image(img)
+
+    result.show()
+
+    return
     args = get_arguments()
 
     gpus = [int(i) for i in args.gpu.split(',')]
@@ -163,6 +175,57 @@ def main():
                 logits_result_path = os.path.join(args.output_dir, img_name[:-4] + '.npy')
                 np.save(logits_result_path, logits_result)
     return
+
+def parse_image(image):
+    dataset = 'lip'
+
+    num_classes = dataset_settings[dataset]['num_classes']
+    input_size = dataset_settings[dataset]['input_size']
+
+    model = networks.init_model('resnet101', num_classes=num_classes, pretrained=None)
+
+    model_restore = 'checkpoints/final.pth'
+    state_dict = torch.load(model_restore)['state_dict']
+    from collections import OrderedDict
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = k[7:]  # remove `module.`
+        new_state_dict[name] = v
+    model.load_state_dict(new_state_dict)
+    model.cuda()
+    model.eval()
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.406, 0.456, 0.485], std=[0.225, 0.224, 0.229])
+    ])
+    indexs = dataset_settings[dataset]['indexs']
+    palette = get_palette(num_classes, indexs)
+
+    dataset = SimpleImageDataset(img=image, input_size=input_size, transform=transform)
+    dataloader = DataLoader(dataset)
+
+    output_img = None
+    with torch.no_grad():
+        for idx, batch in enumerate(tqdm(dataloader)):
+            image, meta = batch
+            c = meta['center'].numpy()[0]
+            s = meta['scale'].numpy()[0]
+            w = meta['width'].numpy()[0]
+            h = meta['height'].numpy()[0]
+
+            output = model(image.cuda())
+            upsample = torch.nn.Upsample(size=input_size, mode='bilinear', align_corners=True)
+            upsample_output = upsample(output[0][-1][0].unsqueeze(0))
+            upsample_output = upsample_output.squeeze()
+            upsample_output = upsample_output.permute(1, 2, 0)  # CHW -> HWC
+
+            logits_result = transform_logits(upsample_output.data.cpu().numpy(), c, s, w, h, input_size=input_size)
+            parsing_result = np.argmax(logits_result, axis=2)
+            output_img = Image.fromarray(np.asarray(parsing_result, dtype=np.uint8))
+            output_img.putpalette(palette)
+
+    return output_img
 
 
 if __name__ == '__main__':
