a14b35eaa5b327a480569c26bc3c5ba2331f24e4
guzhunxin <guzhunxin@dingtalk.com>
2023-05-18 15:36:00 +0800

    stable diffusion web ui扩展

--- scripts/human_parse.py
+++ scripts/human_parse.py
@@ -1,14 +1,36 @@
 import modules.scripts as scripts
 from modules.shared import opts
+import gradio as gr
 from ..simple_extractor import parse_image
 
 
 class HumanParseScript(scripts.Script):
+
+    def __init__(self) -> None:
+        super().__init__()
+
     def title(self):
         return "HumanParse"
 
     def show(self, is_img2img):
         return scripts.AlwaysVisible
+
+    def ui(self, is_img2img):
+        with gr.Accordion('HumanParse', open=False):
+            with gr.Row():
+                angle = gr.Slider(
+                    minimum=0.0,
+                    maximum=360.0,
+                    step=1,
+                    value=0,
+                    label="Angle"
+                )
+                checkbox = gr.Checkbox(
+                    False,
+                    label="Checkbox"
+                )
+        # TODO: add more UI components (cf. https://gradio.app/docs/#components)
+        return [angle, checkbox]
 
     def process(self, p, *args):
         # get orig image
