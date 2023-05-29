05f167e03947054bf8b32013d57b3964354bf0e4
guzhunxin <guzhunxin@dingtalk.com>
2023-05-29 15:46:09 +0800

    参数控制启用

--- scripts/human_parse.py
+++ scripts/human_parse.py
@@ -27,15 +27,26 @@ class HumanParseScript(scripts.Script):
     def check_script_enable(self, p: processing.StableDiffusionProcessing):
 
         try:
+            script_runner = p.scripts
+            if script_runner is None:
+                return False
+
             human_parse_script = None
-            for script in p.scripts.alwayson_scripts:
+            for script in script_runner.alwayson_scripts:
                 if script.title() == 'HumanParse':
                     human_parse_script = script
+
+            if human_parse_script is None:
+                return False
 
             script_args = p.script_args[human_parse_script.args_from:human_parse_script.args_to]
             if script_args is not None and len(script_args) > 0:
                 args = script_args[0]
-                return args['enabled']
+
+                if type(args) is dict:
+                    return args['enabled']
+                else:
+                    return args
 
             return False
 
@@ -45,7 +56,7 @@ class HumanParseScript(scripts.Script):
 
     def process(self, p, *args):
         enable = self.check_script_enable(p)
-        print(f"human parse check_script_enable result, {enable}")
+        print(f"check_script_enable result, {enable}")
         if not enable:
             return
 
